e043971bb1c4b3b8c8cd06b1e06216fe9bc88c40
esmerdel <161901167+esmerdel@users.noreply.github.com>
2024-09-09 22:20:18 -0300

    Update app.blade.php

--- projetobd/resources/views/layouts/app.blade.php
+++ projetobd/resources/views/layouts/app.blade.php
@@ -1,36 +1,30 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+  </head>
+  <body>
+    <main class>
+        <h3> Sistem de GESTÃO estoque - LOGIN</h3>
+        <form action="/login" method="POST">
+            @CSRF
+            <div class="row">
+                <div class="col">
+                    <label for="email" class="form-label"> Informe o email</label>
+                    <input id="email" name="email" class="form-control" type="text">
+                </div>
+                <div class="col">
+                    <label for="password" class="form-label"> Informe a Senha</label>
+                    <input id="password" name="password" class="form-control" type="password">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <button type="submit"> Acessar </button>
+                </div>
+            </div> 
+    </main>
+  </body>
 </html>
